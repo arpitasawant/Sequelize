261f8b26c3ecf3a3dd89fcb05a2e3617c4f8f808
Arpita <arpitasawant2002@gmail.com>
2023-04-24 23:18:51 +0530

    updated

--- models/user.js
+++ models/user.js
@@ -1,26 +1,26 @@
 // const {DataTypes} = require('sequelize')
 // const sequelize = require('./index')
 
-module.exports=(sequelize,DataTypes)=>{
-    const User = sequelize.define('User',{
-    firstName:{
-        type: DataTypes.STRING,
-        //  allowNull:false
-        // defaultValue:'John'
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+            // defaultValue:'John'
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            defaultValue: 'Singh'
+        }
     },
-    lastName:{
-        type: DataTypes.STRING,
-        defaultValue:'Singh'
-    }
-},
-    {
-tableName:'users',
-timestamps:false
-    }
-    
-)
-return User;
-// console.log(User === sequelize.models.User);
+        {
+            tableName: 'users',
+            timestamps: false
+        }
 
-// module.exports = User
+    )
+    return User;
+    // console.log(User === sequelize.models.User);
+
+    // module.exports = User
 }
