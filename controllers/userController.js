9e3bd0c0dbe1a742020535fcbab25fc1cedb93b6
Arpita Sawant <97539573+arpitasawant@users.noreply.github.com>
2023-08-14 22:38:15 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const addUser = async (req,res)=>
 {
     const jane = await User.create({firstName:'Arpita',lastName:'Sawant'});
     console.log(jane instanceof User);//true
-    console.log(jane.firstName);//arpita
+    console.log(jane.firstName);//Arpita
     // await jane.save();
     // jane.set({firstName:'himani',lastName:'Sheregar'});
         jane.update({firstName:'Himani',lastName:'Sheregar'});
